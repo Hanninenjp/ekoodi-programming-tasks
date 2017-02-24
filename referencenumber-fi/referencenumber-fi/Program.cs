115f99352c1f0d0cafb197203d7013a9b2172e81
Hanninenjp <jphanninen@gmail.com>
2017-02-24 16:52:03 +0200

    Using bank-utilities-library, instead of referencenumber-library

--- referencenumber-fi/referencenumber-fi/Program.cs
+++ referencenumber-fi/referencenumber-fi/Program.cs
@@ -11,49 +11,62 @@ namespace referencenumber_fi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bank Reference Number Calculator");
-            Console.WriteLine("Validates and creates Finnish bank reference numbers");
-            Console.WriteLine();
-            Console.WriteLine("1. Validate reference number");
-            Console.WriteLine("2. Create reference number");
-            Console.WriteLine("3. Create sequence of bank reference numbers");
-            Console.WriteLine();
-            Console.Write("Select option: ");
-            string option = Console.ReadLine();
-            Console.WriteLine();
-            if (option == "1")
+            try
             {
-                Console.Write("Enter reference number: ");
-                string referenceNumber = Console.ReadLine();
-                Console.WriteLine("\nValid: {0}", ReferenceNumber.IsValid(referenceNumber));
-            }
-            else if (option == "2")
-            {
-                Console.Write("Enter reference number basepart: ");
-                string referenceBasepart = Console.ReadLine();
-                string referenceNumber = ReferenceCreator.GetReference(referenceBasepart);
-                Console.WriteLine("\nReference number: {0}", referenceNumber);
-            }
-            else if (option == "3")
-            {
-                Console.Write("Enter reference number basepart: ");
-                string referenceBasepart = Console.ReadLine();
-                Console.Write("Enter number of reference numbers: ");
-                //Exception handling could be implemented
-                int referenceCount = int.Parse(Console.ReadLine());
-                IList<string> referenceNumbers = new List<string>();
-                referenceNumbers = ReferenceCreator.GetReference(referenceBasepart, referenceCount);
-                Console.WriteLine("\nReference numbers:");
-                int element = 1;
-                foreach (string reference in referenceNumbers)
+                Console.WriteLine("Bank Reference Number Calculator");
+                Console.WriteLine("Validates and creates Finnish bank reference numbers");
+                Console.WriteLine();
+                Console.WriteLine("1. Validate reference number");
+                Console.WriteLine("2. Create reference number");
+                Console.WriteLine("3. Create sequence of bank reference numbers");
+                Console.WriteLine();
+                Console.Write("Select option: ");
+                string option = Console.ReadLine();
+                Console.WriteLine();
+                if (option == "1")
                 {
-                    Console.WriteLine("{0}. {1}", element, reference);
-                    element++;
+                    Console.Write("Enter reference number: ");
+                    string referenceNumber = Console.ReadLine();
+                    Console.WriteLine("\nValid: {0}", NationalReference.IsValid(referenceNumber));
+                }
+                else if (option == "2")
+                {
+                    Console.Write("Enter reference number basepart: ");
+                    string referenceBasepart = Console.ReadLine();
+                    BankReference reference = ReferenceCreator.GetReference(referenceBasepart);
+                    Console.WriteLine("\nReference number: {0}", reference.ToString());
+                }
+                else if (option == "3")
+                {
+                    Console.Write("Enter reference number basepart: ");
+                    string referenceBasepart = Console.ReadLine();
+                    Console.Write("Enter number of reference numbers: ");
+                    int referenceCount = 0;
+                    try
+                    {
+                        referenceCount = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception)
+                    {
+                        throw new FormatException("Invalid number!");
+                    }
+                    IList<BankReference> references = ReferenceCreator.GetReferenceList(referenceBasepart, referenceCount);
+                    Console.WriteLine("\nReference numbers:");
+                    int element = 1;
+                    foreach (BankReference reference in references)
+                    {
+                        Console.WriteLine("{0}. {1}", element, reference.ToString());
+                        element++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid option!");
                 }
             }
-            else
+            catch (Exception e)
             {
-                Console.WriteLine("Not a valid option!");
+                Console.WriteLine("\n{0}", e.Message);
             }
 
             Console.WriteLine();
