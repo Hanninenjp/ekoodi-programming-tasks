373ef96ad48931fb82f3d15abd797d29699e49b0
Hanninenjp <jphanninen@gmail.com>
2017-02-26 15:43:21 +0200

    Bank virtual barcode generator version for testing

--- bank-utilities-library/bank-utilities/BankBarcode.cs
+++ bank-utilities-library/bank-utilities/BankBarcode.cs
@@ -37,7 +37,7 @@ namespace Ekoodi.Utilities
             }
             else
             {
-                throw new ArgumentException("Bank barcode creation failed: unsupported reference type!");
+                throw new ArgumentException("Bank barcode creation failed:\nUnsupported reference type!");
             }
         }
 
@@ -48,41 +48,20 @@ namespace Ekoodi.Utilities
 
         private static string buildBarcode4(IBAN iban, BankReference reference, Decimal sum, DateTime date)
         {
-            //Format description, just temporarily included for development time purposes
-            //Pankkiviivakoodin symboliversiota 4 käytetään, kun tilinumero on kansainvälisessä
-            //IBAN -muodossa ja viite kansallisessa muodossa.
-            //1. Versionumero 1 4
-            //2. Saajan tilinumeron (IBAN) numeerinen osa 16 N
-            //Viivakoodissa on saajan IBAN -muotoinen tilinumero ilman FI -
-            //maatunnusta.Tilinumero - kentän arvo ei ole koskaan nolla.Viivakoodiin
-            //3. Eurot 6 N
-            //Laskun euromäärä, etunollatäyttö, lukualue 000000... 999999 euroa.Kun
-            //laskun summa ylittää 99999999 senttiä, laskuttaja valintansa mukaan joko
-            //tulostaa euro-ja senttikenttiin arvon 00000000 tai ei lainkaan tulosta
-            //tilisiirtolomakkeelle viivakoodia. Jos maksaja saa itse valita maksettavan
-            //summan, euro - ja senttikenttiin tulostetaan arvo 00000000.
-            //4. Sentit 2 N
-            //Laskun senttimäärä, lukualue 00...99.
-            //5. Varalla 3 000
-            //Tieto on 000 (nolla).
-            //6. Viitenumero(kansallinen) 20 N
-            //Laskun viitenumero (kansallinen), etunollatäyttö. Pankkiviivakoodissa  
-            //viitenumero on aina pakollinen. Etunollia ei tulosteta selkokielisenä
-            //tilisiirtolomakkeelle.
-            //7. Eräpäivä 6 VVKKPP
-            //Laskun eräpäivä on koodissa muodossa vvkkpp, mutta selkokielisenä  
-            //eräpäiväkentässä muodossa pp.kk.vvvv.Laskuttaja voi halutessaan
-            //täyttää pankkiviivakoodin eräpäiväkentän nollilla.
-            //!!!
-            //Note: can be zeroes, perhaps will not be supported, needs attention!
-            //
+            decimal maxSum = (decimal)999999.99;
             string barcode = String.Empty;
             barcode += "4"; //Version 4
             barcode += iban.Iban.Substring(2);
-            //!!!
-            //Check for correct sum range is needed, and better implementation for formatting the sum!
-            //!!!
-            barcode += sum.ToString("000000.00").Replace(",", String.Empty);
+            if (sum >= 0)
+            {
+                //Current implementation sets sum to 0 if the sum exceeds the supported range [0-999999.99]
+                barcode += sum > maxSum ? "00000000" : sum.ToString("000000.00").Replace(",", String.Empty);
+            }
+            else
+            {
+                //Sum cannot be negative
+                throw new ArgumentException(String.Format("Bank barcode creation failed:\nInvalid sum: {0}", sum));
+            }
             barcode += "000"; //Reserved
             barcode += reference.Reference.PadLeft(20, '0');
             barcode += date.ToString("yyMMdd");
@@ -91,25 +70,20 @@ namespace Ekoodi.Utilities
 
         private static string buildBarcode5(IBAN iban, BankReference reference, Decimal sum, DateTime date)
         {
-            //Format description, just temporarily included for development time purposes
-            //Pankkiviivakoodin symboliversiota 5 käytetään, kun tilinumero on kansainvälisessä
-            //IBAN - muodossa ja viite on kansainvälinen RF - viite.
-            //Pankkiviivakoodi rakennetaan vasemmalta oikealle seuraavasti:
-            //Versionumero 1 5
-            //Saajan tilinumeron (IBAN)
-            //numeerinen osa 16 N
-            //Eurot 6 N
-            //Sentit 2 N
-            //RF - viitteen numeerinen osa 23 N
-            //Eräpäivä 6 VVKKPP
-
+            decimal maxSum = (decimal)999999.99;
             string barcode = String.Empty;
             barcode += "5"; //Version 5
             barcode += iban.Iban.Substring(2);
-            //!!!
-            //Check for correct sum range is needed!
-            //!!!
-            barcode += sum.ToString("000000.00").Replace(",", String.Empty);
+            if (sum >= 0)
+            {
+                //Current implementation sets sum to 0 if the sum exceeds the supported range [0-999999.99]
+                barcode += sum > maxSum ? "00000000" : sum.ToString("000000.00").Replace(",", String.Empty);
+            }
+            else
+            {
+                //Sum cannot be negative
+                throw new ArgumentException(String.Format("Bank barcode creation failed:\nInvalid sum: {0}", sum));
+            }
             barcode += "000"; //Reserved
             barcode += reference.Reference.Substring(2, 2) + reference.Reference.Substring(4).PadLeft(21, '0');
             barcode += date.ToString("yyMMdd");
--- barcode-virtual/barcode-virtual/Program.cs
+++ barcode-virtual/barcode-virtual/Program.cs
@@ -1,7 +1,4 @@
-﻿#define DEVELOPMENT
-#define NATIONAL
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,68 +13,41 @@ namespace barcode_virtual
         {
             try
             {
-                //Still under development!
-                //Testing is required!
-                //Better checking and error handling for the entered sum is required!
-                //Exception handling may have some room for improvement
                 Console.WriteLine("Virtual Bank Barcode Generator");
                 Console.WriteLine("Creates Finnish virtual bank barcode");
                 Console.WriteLine("Either national or internation reference can be used");
 
-#if DEVELOPMENT
-                Console.WriteLine("\nDevelopment mode: input values are populated with defaults!");
-                Console.WriteLine("Press Enter in each input to proceed!");
-#endif
-
-                //Get user inputs
                 Console.Write("\nEnter IBAN: ");
                 string ibanInput = Console.ReadLine();
-#if DEVELOPMENT
-                ibanInput = "FI7650940020125924";
-#endif
                 IBAN iban = IBAN.Parse(ibanInput);
 
                 Console.Write("Enter reference: ");
                 string referenceInput = Console.ReadLine();
-#if DEVELOPMENT
-#if INTERNATIONAL
-                referenceInput = "RF8512345672";
-#elif NATIONAL
-                referenceInput = "12345672";
-#elif INVALID
-                referenceInput = "ABC";
-#endif
-#endif
                 BankReference reference = ReferenceCreator.SelectReference(referenceInput);
 
                 Console.Write("Enter sum: ");
                 string sumInput = Console.ReadLine();
-#if DEVELOPMENT
-                sumInput = "123,45";
-#endif
                 decimal sum = decimal.Parse(sumInput);
 
                 Console.Write("Enter due date (DD.MM.YYYY): ");
                 string dateInput = Console.ReadLine();
-
-#if DEVELOPMENT
-                dateInput = "31.03.2017";
-#endif
                 DateTime date = DateTime.Parse(dateInput);
 
                 Console.WriteLine("\nIBAN: {0}", iban.ToString());
                 Console.WriteLine("Reference: {0}", reference.ToString());
-                Console.WriteLine("Reference type: {0}", reference.GetType());
                 Console.WriteLine("Sum: {0} EUR", sum.ToString("F2"));
-                Console.WriteLine("Date: {0}", date.ToString());
+                Console.WriteLine("Date: {0}", date.ToString("dd.MM.yyyy"));
 
                 BankBarcode barcode = new BankBarcode(iban, reference, sum, date);
                 Console.WriteLine("\nVirtual barcode: {0}", barcode.ToString());
             }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(String.Format("\n{0}", e.Message));
+            }
             catch (Exception)
             {
                 Console.WriteLine("\nInvalid input!");
-                //Console.WriteLine("Barcode creation failed!");
             }
             Console.WriteLine("\nPress Enter to exit!");
             Console.ReadLine();
